05c2a58c2622b30de92f802193814a92de21793a
Silvia Hernandez <silviaherf@gmail.com>
2020-08-21 17:52:48 +0200

    Wars class nearly  done

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -57,6 +57,57 @@ class Saxon(Soldier):
 
 # War
 
+import random
 
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy=[]
+        self.saxonArmy=[]
+
+    def addViking(self,oneViking):
+        self.vikingArmy.append(oneViking)
+
+    def addSaxon(self,oneSaxon):
+        self.saxonArmy.append(oneSaxon)
+        
+    def vikingAttack(self):
+        rand_saxon=random.choice(self.saxonArmy)
+        rand_saxon.receiveDamage(oneViking.strength)
+        rand_viking=random.choice(self.vikingArmy)
+        if oneSaxon.health==0:
+            self.saxonArmy.remove(oneSaxon)
+            return oneSaxon.receiveDamage(oneViking.strength)
+
+    def saxonAttack(self):
+        oneViking.receiveDamage(oneSaxon.strength)
+        if oneViking.receiveDamage(oneSaxon.strength)==0:
+            self.vikingsArmy.remove(Viking)
+            return oneViking.receiveDamage(oneSaxon.strength)
+
+    def showStatus(self):
+        if len(self.saxonArmy)==0:
+            return 'Vikings have won the war of the century!'
+        elif len(self.vikingArmy)==0:
+            return 'Saxons have fought for their lives and survive another day...'
+        elif len(self.saxonArmy)>0 or len(self.vikingArmy)>0:
+            return 'Vikings and Saxons are still in the thick of battle.'
+
+
+"""
+vikingAttack() method
+A Saxon (chosen at random) has their receiveDamage() method called with the damage equal to the strength of a Viking (also chosen at random). This should only perform a single attack and the Saxon doesn't get to attack back.
+
+should be a function
+should receive 0 arguments
+should make a Saxon receiveDamage() equal to the strength of a Viking
+should remove dead saxons from the army
+should return result of calling receiveDamage() of a Saxon with the strength of a Viking
+saxonAttack() method
+The Saxon version of vikingAttack(). A Viking receives the damage equal to the strength of a Saxon.
+
+should be a function
+should receive 0 arguments
+should make a Viking receiveDamage() equal to the strength of a Saxon
+should remove dead vikings from the army
+should return result of calling receiveDamage() of a Viking with the strength of a Saxon
+"""
