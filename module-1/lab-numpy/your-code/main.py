f9d6139f471d9f71eb4256bc047dc9b459bc70ac
silviaherf <silviaherf@gmail.com>
2020-08-24 16:12:15 +0200

    14/118 done

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,68 +1,127 @@
+
 #1. Import the NUMPY package under the name np.
+import numpy as np
 
 
 
 #2. Print the NUMPY version and the configuration.
+
+print(np.version.version)
+print(np.show_config())
+
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a=np.random.random((2,3,5))
+print('a=',a)
+
+a2=np.concatenate([np.random.random((3,5)),np.random.random((3,5))])
+print('a2=',a2)
+
+a3=np.random.random((1,5))
+for i in range(0,5):
+        a3=np.concatenate([a3,np.random.random((1,5))])
+print('a3=',a3)
+
 
 
 #4. Print a.
-
+print('a=',a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b=np.ones((5,2,3))
 
 
 #6. Print b.
+
+print('b=',b)
 
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.size==b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
+
+#No porque son de distinto shape! Aunque el núm de elementos sea el mismo
+#ValueError: operands could not be broadcast together with shapes (2,3,5) (5,2,3) 
+
+try:
+        print(a+b)
+except:
+        print(f'ValueError: operands could not be broadcast together with shapes {a.shape} {b.shape}')
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c=b.reshape(2,3,5)
+
+print('c:',c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+try:
+        d=a+c
+        print('d:',d)
+except:
+        print(f'ValueError: operands could not be broadcast together with shapes {a.shape} {b.shape}')
+
+#Funciona porque ahora sí tienen la misma estructura:2 arrays de 3 filasx5columnas
 
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print('a=',a)
+print('d:',d)
 
+#La diferencia es 1, porque a+d hace la suma de cada uno de los elementos de a con el elemento de d en esa misma posición
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e=a*c
+print('e:',e)
+
+#Ha hecho lo mismo:multiplica cada uno de los elementos de a por el elemento de d en esa misma posición
 
 
 #13. Does e equal to a? Why or why not?
 
+if (e.shape==a.shape) & (e.size==a.size) & (e.all()==a.all()):
+        print('Sí, e y a son iguales')
+else:
+        print('No, e y a no son iguales')
 
+#sí son iguales, tienen la misma shape y mismo size y todos sus elementos son iguales
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max=d.max()
+d_min=d.min()
+d_mean=d.mean()
+print("max:",d_max,"min:",d_min,"mean:",d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f=np.empty([2,3,5])
 
+print("f:",f)
+
+
+"""
+"""
 
 
 """
