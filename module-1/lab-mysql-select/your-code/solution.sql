c82e7fe18eac2d068083397c92d9e74088e77b48
Silvia Hernandez <silviaherf@gmail.com>
2020-08-26 20:20:12 +0200

    Bonus attempted

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -66,3 +66,31 @@ LEFT JOIN sales AS sa ON ta.title_id=sa.title_id
 
 GROUP BY au.au_id
 ORDER BY sum(sa.qty) DESC;
+
+
+--BONUS
+--Me temo que no he podido avanzar...no sé relacionar el número de libros vendidos con los % de royalty para poder seguir
+
+SELECT * FROM (SELECT au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", 
+ au.au_fname AS "FIRST NAME", 
+ sum(round(ro.royalty/100*t.price,2)) AS "PROFIT"
+ FROM mydb.titles AS t 
+ 
+JOIN roysched AS ro ON t.title_id=ro.title_id
+JOIN sales AS sa ON t.title_id=sa.title_id
+JOIN titleauthor AS ta ON t.title_id=ta.title_id
+JOIN authors AS au ON ta.au_id=au.au_id
+GROUP BY au.au_id
+ORDER BY sum(round(ro.royalty/100*t.price*sum(t.ytd_sales),2)) DESC) as royalty
+
+JOIN (SELECT au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", 
+ au.au_fname AS "FIRST NAME", 
+ sum(t.ytd_sales) AS "TOTAL"
+ FROM mydb.titles AS t 
+ 
+LEFT JOIN sales AS sa ON t.title_id=sa.title_id
+RIGHT JOIN titleauthor AS ta ON t.title_id=ta.title_id
+RIGHT JOIN authors AS au ON ta.au_id=au.au_id
+GROUP BY au.au_id
+ORDER BY sum(t.ytd_sales) DESC) as titles ON ro.title_id=t.title_id
+;
