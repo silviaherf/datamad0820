29ef9b6b989d4de7a2b1c71ec2cc4ddff09fee86
Silvia Hernandez <silviaherf@gmail.com>
2020-08-26 17:37:42 +0200

    Exercisse 2/5 done

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -7,8 +7,24 @@
 
 INNER JOIN publishers AS pub ON t.pub_id=pub.pub_id
 INNER JOIN titleauthor AS ta ON t.title_id=ta.title_id
-INNER JOIN authors AS au ON ta.au_id=au.au_id;
+INNER JOIN authors AS au ON ta.au_id=au.au_id
+ORDER BY au.au_id DESC;;
 
 #Challenge 2
 
+SELECT au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", 
+ au.au_fname AS "FIRST NAME", t.title AS "TITLE",
+ pub.pub_name AS "PUBLISHER", COUNT(*) as "TITLE COUNT"
+ FROM mydb.titles AS t 
+INNER JOIN publishers AS pub ON t.pub_id=pub.pub_id
+INNER JOIN titleauthor AS ta ON t.title_id=ta.title_id
+INNER JOIN authors AS au ON ta.au_id=au.au_id
+GROUP BY au.au_id, au.au_lname, au.au_fname,
+t.title,  pub.pub_name
+ORDER BY COUNT(*) DESC;
 
+
+
+
+
+#Challenge 3
