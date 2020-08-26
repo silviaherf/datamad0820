43141a1e88dc521e3c3821cead47e64634bad30a
Silvia Hernandez <silviaherf@gmail.com>
2020-08-26 17:50:01 +0200

    Exercisse 3/5 done

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -16,15 +16,24 @@ SELECT au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME",
  au.au_fname AS "FIRST NAME", t.title AS "TITLE",
  pub.pub_name AS "PUBLISHER", COUNT(*) as "TITLE COUNT"
  FROM mydb.titles AS t 
+
 INNER JOIN publishers AS pub ON t.pub_id=pub.pub_id
 INNER JOIN titleauthor AS ta ON t.title_id=ta.title_id
 INNER JOIN authors AS au ON ta.au_id=au.au_id
-GROUP BY au.au_id, au.au_lname, au.au_fname,
-t.title,  pub.pub_name
-ORDER BY COUNT(*) DESC;
-
-
-
+GROUP BY t.title
+ORDER BY au.au_id DESC, count(*) DESC ;
 
 
 #Challenge 3
+
+SELECT au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", 
+ au.au_fname AS "FIRST NAME", t.title AS "TITLE",
+  COUNT(*) as "TOTAL"
+ FROM mydb.titles AS t 
+
+
+INNER JOIN titleauthor AS ta ON t.title_id=ta.title_id
+INNER JOIN authors AS au ON ta.au_id=au.au_id
+GROUP BY t.title
+ORDER BY count(*) DESC 
+LIMIT 3;
