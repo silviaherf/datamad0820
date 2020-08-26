3397d5ce32bfa5a9ae19d6c59eab8ec80627e5bd
Silvia Hernandez <silviaherf@gmail.com>
2020-08-26 18:02:51 +0200

    Exercisse 3/5 well done

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -27,13 +27,12 @@ ORDER BY au.au_id DESC, count(*) DESC ;
 #Challenge 3
 
 SELECT au.au_id AS "AUTHOR ID", au.au_lname AS "LAST NAME", 
- au.au_fname AS "FIRST NAME", t.title AS "TITLE",
-  COUNT(*) as "TOTAL"
+ au.au_fname AS "FIRST NAME", t.title AS "TITLE", 
+ sum(t.ytd_sales) AS "TOTAL"
  FROM mydb.titles AS t 
-
 
 INNER JOIN titleauthor AS ta ON t.title_id=ta.title_id
 INNER JOIN authors AS au ON ta.au_id=au.au_id
-GROUP BY t.title
-ORDER BY count(*) DESC 
+GROUP BY au.au_id
+ORDER BY sum(t.ytd_sales) DESC 
 LIMIT 3;
